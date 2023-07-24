e924b38425e7cab940df4959f552edc543837d8e
pulse <antynomia@gmail.com>
2023-07-24 17:30:03 +0200

    alphav1

--- controllers/clusterassignment_controller.go
+++ controllers/clusterassignment_controller.go
@@ -1,49 +1,3 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package controllers
-
-import (
-	"context"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"strings"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	managementv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-)
-
-// ClusterAssignmentReconciler reconciles a ClusterAssignment object
-type ClusterAssignmentReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
-}
-
-//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments/finalizers,verbs=update
-//+kubebuilder:rbac:groups=management.cattle.io,resources=users,verbs=get;list;watch;update;patch
-//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
 //
 //	func (r *ClusterAssignmentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 //		// Fetch the User instance
@@ -81,6 +35,51 @@ type ClusterAssignmentReconciler struct {
 //
 //		return ctrl.Result{}, nil
 //	}
+
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
+
+	managementv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+// ClusterAssignmentReconciler reconciles a ClusterAssignment object
+type ClusterAssignmentReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=permissions.xddevelopment.com,resources=clusterassignments/finalizers,verbs=update
+//+kubebuilder:rbac:groups=management.cattle.io,resources=users,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=management.cattle.io,resources=clusters,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+
 func (r *ClusterAssignmentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	// Fetch the User instance
 	user := &managementv3.User{}
@@ -100,29 +99,14 @@ func (r *ClusterAssignmentReconciler) Reconcile(ctx context.Context, req ctrl.Re
 
 	// Check if the user is being deleted
 	if user.DeletionTimestamp != nil {
-		// The object is being deleted
-		if contains(user.GetFinalizers(), userFinalizer) {
-			// our finalizer is present, so lets handle any external dependency
-			if err := r.deleteUserSecret(ctx, secretName, secretNamespace); err != nil {
-				// if fail to delete the external dependency here, return with error
-				// so that it can be retried
-				return ctrl.Result{}, err
-			}
-
-			// remove our finalizer from the list and update it.
-			user.SetFinalizers(remove(user.GetFinalizers(), userFinalizer))
-			if err := r.Update(context.Background(), user); err != nil {
-				return ctrl.Result{}, err
-			}
-		}
-		return ctrl.Result{}, nil
-	}
-
-	// Add finalizer for this CR
-	if !contains(user.GetFinalizers(), userFinalizer) {
-		if err := r.addFinalizer(ctx, user); err != nil {
+		// The user is being deleted
+		if err := r.deleteUserSecret(ctx, secretName, secretNamespace); err != nil {
+			// if fail to delete the external dependency here, return with error
+			// so that it can be retried
 			return ctrl.Result{}, err
 		}
+
+		return ctrl.Result{}, nil
 	}
 
 	// Check if the secret already exists
@@ -152,19 +136,32 @@ func (r *ClusterAssignmentReconciler) Reconcile(ctx context.Context, req ctrl.Re
 		}
 	}
 
-	// No error occurred
+	// Check the user's attributes or groups to decide which clusters they should have access to.
+	clusters, err := determineClustersForUser(ctx, r, user)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	for clusterName, namespace := range clusters {
+		// Create a ClusterRoleTemplateBinding for each cluster the user should have access to.
+		binding := &managementv3.ClusterRoleTemplateBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      user.Name + "-" + clusterName,
+				Namespace: namespace,
+			},
+			RoleTemplateName:  "cluster-owner",
+			UserName:          user.Name,
+			UserPrincipalName: user.PrincipalIDs[0],
+		}
+
+		// Apply the ClusterRoleTemplateBinding to the cluster.
+		if err := r.Create(ctx, binding); err != nil {
+			// handle error
+			return ctrl.Result{}, client.IgnoreNotFound(err)
+		}
+	} // No error occurred
 	return ctrl.Result{}, nil
 }
-
-func (r *ClusterAssignmentReconciler) addFinalizer(ctx context.Context, user *managementv3.User) error {
-	user.SetFinalizers(append(user.GetFinalizers(), userFinalizer))
-	// Update CR
-	return r.Update(ctx, user)
-}
-
-const (
-	userFinalizer = "user.finalizers.permissions.xddevelopment.com"
-)
 
 func (r *ClusterAssignmentReconciler) deleteUserSecret(ctx context.Context, secretName, secretNamespace string) error {
 	// delete secret
@@ -184,24 +181,6 @@ func (r *ClusterAssignmentReconciler) deleteUserSecret(ctx context.Context, secr
 	return err
 }
 
-func contains(list []string, s string) bool {
-	for _, v := range list {
-		if v == s {
-			return true
-		}
-	}
-	return false
-}
-
-func remove(list []string, s string) (result []string) {
-	for _, v := range list {
-		if v != s {
-			result = append(result, v)
-		}
-	}
-	return
-}
-
 // SetupWithManager sets up the controller with the Manager.
 func (r *ClusterAssignmentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
@@ -209,34 +188,21 @@ func (r *ClusterAssignmentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func determineClustersForUser(ctx context.Context, r *ClusterAssignmentReconciler, user *managementv3.User) ([]string, error) {
-	// Find the user's group
-	group := ""
-	for _, principalId := range user.PrincipalIDs {
-		if strings.Contains(principalId, "eks-dev-team-") {
-			group = strings.Replace(principalId, "eks-", "", 1)
-			break
-		}
-	}
-
-	// If the user isn't in a relevant group, they don't get access to any clusters
-	if group == "" {
-		return nil, nil
-	}
-
-	// Fetch all clusters
-	clusters := &managementv3.ClusterList{}
-	if err := r.List(ctx, clusters); err != nil {
+func determineClustersForUser(ctx context.Context, r *ClusterAssignmentReconciler, user *managementv3.User) (map[string]string, error) {
+	var clusterList managementv3.ClusterList
+	if err := r.List(ctx, &clusterList); err != nil {
 		return nil, err
 	}
 
-	// Filter the clusters based on their names
-	result := []string{}
-	for _, cluster := range clusters.Items {
-		if strings.Contains(cluster.Name, group) {
-			result = append(result, cluster.Name)
+	clusters := make(map[string]string)
+	for _, cluster := range clusterList.Items {
+		labels := cluster.ObjectMeta.GetLabels()
+		if ownerLabel, ok := labels["owner"]; ok {
+			if strings.Contains(ownerLabel, user.Username) {
+				clusters[cluster.Name] = cluster.Namespace
+			}
 		}
 	}
 
-	return result, nil
+	return clusters, nil
 }
