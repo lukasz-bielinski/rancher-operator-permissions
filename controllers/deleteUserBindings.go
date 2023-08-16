54ba51dcfdb06c9121c26052a226f42a83121f55
pulse <antynomia@gmail.com>
2023-08-16 18:05:56 +0200

    alphav1

--- controllers/deleteUserBindings.go
+++ controllers/deleteUserBindings.go
@@ -18,22 +18,33 @@ func (r *ClusterAssignmentReconciler) deleteUserBindings(ctx context.Context, Us
 	// Filter out objects based on annotations and name
 	toDelete := []*managementv3.ClusterRoleTemplateBinding{}
 	for _, binding := range bindingList.Items {
+		globalLog.Info("Checking binding", "UserName", binding.UserName, "Annotations", binding.Annotations)
+
 		if binding.UserName == Username {
 			annotationValue, hasAnnotation := binding.Annotations["created-by-pod"]
 			if hasAnnotation && annotationValue == "rancher-operator-permissions-controller-manager" {
 				toDelete = append(toDelete, &binding)
+			} else if !hasAnnotation {
+				globalLog.Info("Binding has no 'created-by-pod' annotation", "BindingName", binding.Name)
+			} else {
+				globalLog.Info("Binding annotation doesn't match expected value", "BindingName", binding.Name, "AnnotationValue", annotationValue)
 			}
+		} else {
+			globalLog.Info("Binding UserName doesn't match the given UserName", "BindingUserName", binding.UserName, "GivenUserName", Username)
 		}
 	}
 
-	// Now, we'll delete the filtered objects
+	globalLog.Info("Number of ClusterRoleTemplateBindings to delete", "count", len(toDelete))
+
 	for _, binding := range toDelete {
-		globalLog.Info("Attempting to delete ClusterRoleTemplateBinding", "name", binding.Name, "namespace", binding.Namespace)
 		if err := r.Delete(ctx, binding); err != nil {
-			globalLog.Error(err, "Error deleting ClusterRoleTemplateBinding", "name", binding.Name, "namespace", binding.Namespace)
+			globalLog.Info("Error deleting ClusterRoleTemplateBinding", "name", binding.Name, "namespace", binding.Namespace, "error", err)
 			return ctrl.Result{}, err
+		} else {
+			globalLog.Info("Successfully deleted ClusterRoleTemplateBinding", "name", binding.Name, "namespace", binding.Namespace)
 		}
 	}
 
+	globalLog.Info("Exiting deleteUserBindings method...")
 	return ctrl.Result{}, nil
 }
